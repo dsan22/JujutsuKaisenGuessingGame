2929346530ac223bcb0b4499ce0796d368c5de94
dsan22 <jankovicdusan00@gmail.com>
2025-01-04 00:03:28 +0100

    added basic table for displaying characters

--- resources/views/admin/characters.blade.php
+++ resources/views/admin/characters.blade.php
@@ -1,3 +1,5 @@
 <x-app-layout>
-    test
+    <div class=" py-12 max-w-7xl mx-auto sm:px-6 lg:px-8 overflow-hidden">
+        <livewire:characters.characters_table />
+    </div>
 </x-app-layout>
---
+++ resources/views/livewire/characters/characters_table.blade.php
@@ -0,0 +1,43 @@
+<?php
+
+use Livewire\Volt\Component;
+use App\Models\Character;
+
+new class extends Component {
+    public $characters;
+    public function mount() {
+        $this->characters = Character::all();
+    }
+}; ?>
+
+<div>
+    <table class="w-full divide-y divide-gray-200 m-2">
+        <thead class="uppercase bg-gray-300 text-gray-800">
+            <tr>
+                <th>Name</th>
+                <th>Gender</th>
+                <th>Alive</th>
+                <th>Ark Introduced At</th>
+                <th>Grade</th>
+                <th>Have Domain Expansion</th>
+                <th>Have Reverse Curse Technique</th>
+                <th>Used Black Flash</th>
+            </tr>
+           
+        </thead>
+        <tbody>
+            @foreach ($characters as $character)
+                <tr>
+                    <td>{{$character->name}}</td>
+                    <td>{{$character->gender}}</td>
+                    <td>{{$character->is_alive}}</td>
+                    <td>{{$character->ark->name}} </td>
+                    <td>{{$character->grade->name}}</td>
+                    <td>{{$character->have_domain_expansion }}</td>
+                    <td>{{$character->have_reverse_cursed_technique }}</td>
+                    <td>{{$character->used_black_flash }}</td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
