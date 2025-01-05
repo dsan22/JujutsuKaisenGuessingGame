1166bbe9c7375dafb8df4c10312fa9081a428e10
dsan22 <jankovicdusan00@gmail.com>
2025-01-05 15:25:36 +0100

    added badges for bollean types and buton for deleting character

--- resources/views/livewire/characters/characters_table.blade.php
+++ resources/views/livewire/characters/characters_table.blade.php
@@ -15,34 +15,68 @@ new class extends Component {
     public function refreshCharacters() {
         $this->characters = Character::all();
     }
+    public function deleteCharacter(Character $character) {
+        $character->delete();
+        $this->refreshCharacters();
+    }
 }; ?>
 
 <div>
     <table class="w-full divide-y divide-gray-200 m-2">
-        <thead class="uppercase bg-gray-300 text-gray-800">
+        <thead class="text-sm uppercase bg-gray-300 text-gray-800">
             <tr>
                 <th>Name</th>
                 <th>Gender</th>
                 <th>Alive</th>
                 <th>Ark Introduced At</th>
                 <th>Grade</th>
-                <th>Have Domain Expansion</th>
-                <th>Have Reverse Curse Technique</th>
-                <th>Used Black Flash</th>
+                <th>Domain Expansion</th>
+                <th>Reverse Curse Technique</th>
+                <th>Black Flash</th>
+                <th>Action</th>
             </tr>
            
         </thead>
         <tbody>
             @foreach ($characters as $character)
-                <tr>
+                <tr class="text-center">
                     <td>{{$character->name}}</td>
-                    <td>{{$character->gender}}</td>
-                    <td>{{$character->is_alive}}</td>
+                    <td class="capitalize">{{$character->gender}}</td>
+                    <td>
+                        @if($character->is_alive)
+                        <x-badge rounded positive label="Alive" />
+                        @else
+                        <x-badge rounded negative label="Dead" />
+                        @endif
+                    </td>
                     <td>{{$character->ark->name}} </td>
                     <td>{{$character->grade->name}}</td>
-                    <td>{{$character->have_domain_expansion }}</td>
-                    <td>{{$character->have_reverse_cursed_technique }}</td>
-                    <td>{{$character->used_black_flash }}</td>
+                    <td>@if($character->have_domain_expansion) 
+                            <x-badge rounded positive label="Have" />
+                         @else 
+                            <x-badge rounded negative label="Doesn't have" /> 
+                         @endif
+                    </td>
+                    <td>
+                        @if($character->have_reverse_cursed_technique) 
+                            <x-badge rounded positive label="Have" /> 
+                        @else 
+                            <x-badge rounded negative label="Doesn't have" /> 
+                        @endif
+                    </td>
+                    <td>
+                        @if($character->used_black_flash) 
+                            <x-badge rounded positive label="Used" /> 
+                        @else 
+                            <x-badge rounded negative label="Didn't used" /> 
+                        @endif
+                    </td>
+                    <td>
+                        <div class="flex justify-between gap-4">
+                            <x-button primary label="Edit" wire:click="editCharacter({{$character->id}})" />
+                            <x-button negative label="Delete" wire:click="deleteCharacter({{$character->id}})" />
+                        </div>
+                    </td>
                 </tr>
             @endforeach
         </tbody>
